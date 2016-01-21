e70f446f17ba0b01f7dbc5c9ac35aaf67994e87f
Lane <zhanglintc623@gmail.com>
2016-01-21 18:25:33 +0800

    client.rb

--- client/client.rb
+++ client/client.rb
@@ -2,13 +2,24 @@
 # encoding = utf-8
 require 'net/http'
 
-Net::HTTP.start('127.0.0.1', 2603) do |http|
-  response = http.get('/')
-  head_hash = response.to_hash
-  head_hash.keys.each do |key|
-      puts key.to_s + ': ' + head_hash[key].to_s
-  end
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
 
-  puts response.body
-end
+require 'json'
 
+s = open("E:/Git_Mine/Mmrz/vocnw/word-ori.mmz", "rb").read
+puts "sending"
+
+uri = URI('http://115.29.192.240:2603')
+uri = URI.parse 'http://zhanglin.work:2603'
+params = {}  
+params["q"] = s 
+res = Net::HTTP.post_form(uri, params)   
+puts res.header['set-cookie']  
+puts res.body
+
+# http = Net::HTTP.new(uri.host, uri.port)
+# req = Net::HTTP::Post.new(uri.request_uri)
+# req.set_form_data("q"=>s)
+# response = http.request(req)
+# puts response.body
