1b16bb88732cc8920715008f8d8521877ffa6006
zhanglintc <zhanglintc623@gmail.com>
2016-11-14 18:20:39 +0800

    add bottle.py & Mmrz-Sync.py

---
+++ server/Mmrz-Sync.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bottle import route, run, template
+
+@route('/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+run(host='localhost', port=3516)
