826c60ba4521c885611791378968238eb9d5b1e4
zhanglintc <zhanglintc623@gmail.com>
2016-11-15 23:44:44 +0800

    use @post and @get

--- server/Mmrz-Sync.py
+++ server/Mmrz-Sync.py
@@ -6,7 +6,8 @@
 # GET mothod params: request.params
 # POST mothod params: request.forms
 
-from bottle import route, run, template, request
+from bottle import route, run, template
+from bottle import post, get, request
 from db import MmrzSyncDBManager
 import configparser
 import json, sys
@@ -36,13 +37,43 @@ def read_version():
 
     return CLI_VERSION, GUI_VERSION
 
-### routes
-@route('/')
+def is_username_available(username):
+    dbMgr = MmrzSyncDBManager("USERS")
+    users = dict(dbMgr.read_USERS_DB())
+    dbMgr.closeDB()
+
+    return not username in users
+
+def verify_login(username, password):
+    dbMgr = MmrzSyncDBManager("USERS")
+    users = dict(dbMgr.read_USERS_DB())
+    dbMgr.closeDB()
+
+    return username in users and password == users[username]
+
+### posts
+@post('/log_in')
+def log_in():
+    username = request.forms['username']
+    password = request.forms['password']
+
+    dict_for_return = dict(universal_POST_dict)
+    if verify_login(username, password):
+        dict_for_return['verified'] = True
+        dict_for_return['message_str'] = "logged in"
+    else:
+        dict_for_return['verified'] = False
+        dict_for_return['message_str'] = "username or password not correct"
+
+    json_for_return = json.dumps(dict_for_return)
+    return json_for_return
+
+### gets
+@get('/')
 def index():
     return "Hello, world!!!"
 
-
-@route('/version_info')
+@get('/version_info')
 def version_info():
     cli, gui = read_version()
 
@@ -50,6 +81,14 @@ def version_info():
     version['version_info'] = {"CLI": cli, "GUI": gui}
 
     return json.dumps(version)
+
+@get('/database_info')
+def database_info(environ):
+    dbMgr = MmrzSyncDBManager("wordbook")
+    rows = dbMgr.readAllDB()
+    dbMgr.closeDB()
+
+    return json.dumps(rows)
 
 
 run(host='localhost', port=PORT)
