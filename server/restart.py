e5eb324b2c75b4ef780c307e8c6a358d962bd1d9
Lane <zhanglintc623@gmail.com>
2016-11-17 22:59:58 +0800

    change app_name in restart.py

--- server/restart.py
+++ server/restart.py
@@ -1,7 +1,9 @@
 import os
 
-print("killing server.py")
-os.system("ps -ef | grep server.py | grep -v grep | cut -c 9-15 | xargs kill -s 9")
+app_name = "Mmrz-Sync.py"
 
-print("restarting server.py")
-os.system("nohup python server.py&")
+print("killing {0}".format(app_name))
+os.system("ps -ef | grep {0} | grep -v grep | cut -c 9-15 | xargs kill -s 9".format(app_name))
+
+print("restarting {0}".format(app_name))
+os.system("nohup python {0}&".format(app_name))
