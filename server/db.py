e86e3688b3c6ed763a6daec8d0704b5bc53663ce
zhanglintc <zhanglintc623@gmail.com>
2017-03-16 10:40:38 +0800

    replace SQL statement

--- server/db.py
+++ server/db.py
@@ -139,7 +139,9 @@ class MmrzSyncDBManager:
         self.c.execute("VACUUM")
 
     def getNearestRemindRow(self):
-        return self.c.execute("select * from UNMMRZ as tab1, (select min(remindTime) as rem from UNMMRZ  where memTimes < 8) as tab2 where tab1.memTimes < 8 and tab1.remindTime = tab2.rem").fetchall()
+        # old SQL by yanbin:
+        # select * from UNMMRZ as tab1, (select min(remindTime) as rem from UNMMRZ where memTimes < 8) as tab2 where tab1.memTimes < 8 and tab1.remindTime = tab2.rem
+        return self.c.execute("select * from UNMMRZ where memTimes < 8 order by remindTime asc limit 1").fetchall()
 
     def getMaxWordID(self):
         # format of maxWordID is like: maxWordID = [[33]], thus use maxWordID[0][0] to access it
