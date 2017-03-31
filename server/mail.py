d84cce2baf9918d6eac4a77308598269687cb51a
zhanglintc <zhanglintc623@gmail.com>
2017-03-31 10:21:56 +0800

    add parameters

--- server/mail.py
+++ server/mail.py
@@ -23,17 +23,36 @@ fr = open("mailInfo.json", "rb")
 mailInfo = json.loads(fr.read())
 fr.close()
 
-def send_mail():
-    smtp = SMTP_SSL(mailInfo["hostname"].encode("utf-8"))
-    smtp.set_debuglevel(0)
-    smtp.ehlo(mailInfo["hostname"].encode("utf-8"))
-    smtp.login(mailInfo["username"].encode("utf-8"), mailInfo["password"].encode("utf-8"))
+def send_mail(
+        p_from = None,
+        p_to = None,
+        p_hostname = None,
+        p_username = None,
+        p_password = None,
+        p_mailsubject = None,
+        p_mailtext = None,
+        p_mailencoding = None
+    ):
+    p_from = p_from or mailInfo["from"].encode("utf-8")
+    p_to = p_to or mailInfo["to"].encode("utf-8")
+    p_hostname = p_hostname or mailInfo["hostname"].encode("utf-8")
+    p_username = p_username or mailInfo["username"].encode("utf-8")
+    p_password = p_password or mailInfo["password"].encode("utf-8")
+    p_mailsubject = p_mailsubject or mailInfo["mailsubject"].encode("utf-8")
+    p_mailtext = p_mailtext or mailInfo["mailtext"].encode("utf-8")
+    p_mailencoding = p_mailencoding or mailInfo["mailencoding"].encode("utf-8")
+
+    smtp = SMTP_SSL(p_hostname)
+    smtp.set_debuglevel(1)
+    smtp.ehlo(p_hostname)
+    smtp.login(p_username, p_password)
     
-    msg = MIMEText(mailInfo["mailtext"], "plain", mailInfo["mailencoding"].encode("utf-8"))
-    msg["Subject"] = Header(mailInfo["mailsubject"].encode("utf-8"), mailInfo["mailencoding"].encode("utf-8"))
-    msg["from"] = mailInfo["from"].encode("utf-8")
-    msg["to"] = mailInfo["to"].encode("utf-8")
-    smtp.sendmail(mailInfo["from"].encode("utf-8"), mailInfo["to"].encode("utf-8"), msg.as_string())
+    msg = MIMEText(p_mailtext, "plain", p_mailencoding)
+    msg["Subject"] = Header(p_mailsubject, p_mailencoding)
+    msg["from"] = p_from
+    msg["to"] = p_to
+
+    smtp.sendmail(p_from, p_to, msg.as_string())
     
     smtp.quit()
         
