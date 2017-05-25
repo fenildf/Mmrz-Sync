82dc6390f64563948129fc4a0ec9bfdb5c3a4e20
zhanglintc <zhanglintc623@gmail.com>
2017-05-25 09:40:17 +0800

    use paste again

--- server/Mmrz-Sync.py
+++ server/Mmrz-Sync.py
@@ -1138,6 +1138,6 @@ print ""
 # import gevent; from gevent import monkey; monkey.patch_all()
 
 # run server
-run(host='0.0.0.0', port=PORT)#, server='paste')
+run(host='0.0.0.0', port=PORT, server='paste')
 
 
