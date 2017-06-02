e7a7d8eb8e149ff4077a211a41ca51257da2c15d
zhanglintc <zhanglintc623@gmail.com>
2017-06-02 18:12:18 +0800

    add MongoDBManager.py

---
+++ server/MongoDBManager.py
@@ -0,0 +1,29 @@
+#!/env/bin/python
+# encoding: utf-8
+
+import pymongo
+
+# Database: Mmrz-Sync
+# 
+# Colletions:
+# 1. memorize_state
+
+class MongoDBManager:
+    def __init__(self):
+        self.client = pymongo.MongoClient()
+        self.client.admin.authenticate("lane", "lane", mechanism="SCRAM-SHA-1")
+        self.db = self.client["Mmrz-Sync"]
+
+    def update_memorize_state(self, document={}):
+        result = self.db.memorize_state.find(document["username"])
+
+        # if not find, insert document
+        if result.count() == 0:
+            self.db.memorize_state.insert(document)
+        # if find, update codument
+        else:
+            self.db.memorize_state.update({"username": document["username"]}, {"$set": document})
+
+MDBManager = MongoDBManager()
+
+
