07157f05e52dd08d19362a195f82024f48291009
zhanglintc <zhanglintc623@gmail.com>
2017-12-22 00:14:57 +0800

    v=1026

--- server/Mmrz-Sync.py
+++ server/Mmrz-Sync.py
@@ -28,7 +28,7 @@ import datetime, time, math
 import re
 import os
 
-static_file_verion = 'v=1025'
+static_file_verion = 'v=1026'
 
 def each_file(target):
     for root, dirs, files in os.walk(target):
